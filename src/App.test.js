64f52a7ea135808488db343e94ad0dc552ce55fc
yuriko-pixel <beljar731@gmail.com>
2021-08-10 21:09:36 +0900

    Started testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, {shallow} from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import React from 'react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without error', () => {
+  const wrapper = shallow(<App/>);
+  
+});
+
+test('renders without error', () => {
+
+});
+
+test('renders without error', () => {
+
+});
+
+test('renders without error', () => {
+
+});
+
+test('renders without error', () => {
+
 });
